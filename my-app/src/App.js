f82a3beb4fb397c7f4f0c3dc8ae347c33397ead3
Macbook Air <macbookair@MacBook-Air-de-Macbook.local>
2022-03-05 10:20:02 +0100

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Stack from 'react-bootstrap/Stack'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container,Row,Col,Button,Alert,Breadcrumb,Card,Form}from
+'react-bootstrap'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App">
+       <h1>hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
